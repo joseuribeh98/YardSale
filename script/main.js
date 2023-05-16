01fd2e8bf0383688f68236647d897def6ca634a6
Jose Uribe <joseuribeh@gmail.com>
2023-05-15 20:26:56 -0500

    mobileMenu JS added & CSS updated

--- script/main.js
+++ script/main.js
@@ -1,7 +1,16 @@
 const navBarEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const hamburgerMenu = document.querySelector(".menu")
+const mobileMenu = document.querySelector(".mobile-menu")
+
+mobileMenu.classList.remove('visible')
 
 navBarEmail.addEventListener("click", toggleDesktopMenu);
 function toggleDesktopMenu() {
   desktopMenu.classList.toggle("inactive");
+}
+
+hamburgerMenu.addEventListener("click", toggleMobileMenu);
+function toggleMobileMenu() {
+  mobileMenu.classList.toggle("visible");
 }
